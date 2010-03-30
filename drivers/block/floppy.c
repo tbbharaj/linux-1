e90be7e620a244ab57168aa58013f7eec194807d
Kyle McMartin <kyle@phobos.i.jkkm.org>
2010-03-30 00:04:29 -0400

    die-floppy-die
    Kill the floppy.ko pnp modalias. We were surviving just fine without
    autoloading floppy drivers, tyvm.
    
    Please feel free to register all complaints in the wastepaper bin.

--- drivers/block/floppy.c
+++ drivers/block/floppy.c
@@ -4602,8 +4602,7 @@ static const struct pnp_device_id floppy_pnpids[] = {
 	{"PNP0700", 0},
 	{}
 };
-
-MODULE_DEVICE_TABLE(pnp, floppy_pnpids);
+/* MODULE_DEVICE_TABLE(pnp, floppy_pnpids); */
 
 #else
 
