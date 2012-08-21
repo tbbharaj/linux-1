744109d8bae160c997055e4c038f2915dfb6c598
RPM Patch <kernel@fedoraproject.org>
2012-08-21 22:28:43 +0000

    [fedora] patch debug-808990
     net/ipv4/route.c |    8 ++++++++
     1 files changed, 8 insertions(+), 0 deletions(-)
      --- linux-3.3.noarch/net/ipv4/route.c~	2012-04-02 20:24:44.162789023 -0400
    
    Signed-off-by: Cristian Gafton <gafton@amazon.com>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -2793,6 +2793,14 @@ static struct rtable *ip_route_output_slow(struct net *net, struct flowi4 *fl4)
 		fl4->saddr = FIB_RES_PREFSRC(net, res);
 
 	dev_out = FIB_RES_DEV(res);
+
+	if (!dev_out) {
+		pr_crit("ipv4: FIB_RES_DEV() is NULL, nh_sel=%d\n",
+			res.nh_sel);
+		rth = ERR_PTR(-EINVAL);
+		goto out;
+	}
+
 	fl4->flowi4_oif = dev_out->ifindex;
 
 
