e376e57891af2b04d7a305eac3562da89b7879fa
Dave Anderson <anderson@redhat.com>
2013-11-26 12:42:46 -0500

    crash-driver
    Bugzilla: N/A
    Upstream-status: Fedora mustard

--- arch/s390/mm/maccess.c
+++ arch/s390/mm/maccess.c
@@ -197,6 +197,7 @@ void *xlate_dev_mem_ptr(phys_addr_t addr)
 	put_online_cpus();
 	return bounce;
 }
+EXPORT_SYMBOL_GPL(xlate_dev_mem_ptr);
 
 /*
  * Free converted buffer for /dev/mem access (if necessary)
@@ -206,3 +207,4 @@ void unxlate_dev_mem_ptr(phys_addr_t addr, void *buf)
 	if ((void *) addr != buf)
 		free_page((unsigned long) buf);
 }
+EXPORT_SYMBOL_GPL(unxlate_dev_mem_ptr);
