0b9c68e9db8c138c1c73a5e9ad02ed62f6810938
Cristian Gafton <gafton@amazon.com>
2014-02-21 19:45:15 +0000

    we no longer use BASEREL

--- scripts/import-srpm.sh
+++ scripts/import-srpm.sh
@@ -167,12 +167,7 @@ export PATH=$TOPDIR/scripts:$PATH
 rpmbuild "${rpmopts[@]}" "${RPM_PREP[@]}" \
     --define "$(spec_prep_post)" \
     -bp --nodeps --target=x86_64 $SPECFILE
-# add the tag we'd like to use to tag this import
-_rel=$(rpm -qp --qf '%{RELEASE}' $SRPM 2>/dev/null)
-cat >>linux.vers <<EOF
-linux_BASEREL   = ${_rel%%.fc*}
-EOF
-git add linux.vers
 
+git add linux.vers
 git commit --allow-empty --quiet -m "imported source rpm ${TAG}"
 say_green "Import of $SRPM completed"
