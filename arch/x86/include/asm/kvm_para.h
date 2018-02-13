038b1ec41ce562d45ab8a7e7d68a6bde85871bc3
Wanpeng Li <wanpengli@tencent.com>
2018-02-13 09:05:40 +0800

    KVM: Introduce paravirtualization hints and KVM_HINTS_DEDICATED
    This patch introduces kvm_para_has_hint() to query for hints about
    the configuration of the guests.  The first hint KVM_HINTS_DEDICATED,
    is set if the guest has dedicated physical CPUs for each vCPU (i.e.
    pinning and no over-commitment).  This allows optimizing spinlocks
    and tells the guest to avoid PV TLB flush.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Radim Krčmář <rkrcmar@redhat.com>
    Cc: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Wanpeng Li <wanpengli@tencent.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>
    Reviewed-by: Vallish Vaidyeshwara <vallish@amazon.com>
    Reviewed-by: Frank van der Linden <fllinden@amazon.com>
    Signed-off-by: Eduardo Valentin <eduval@amazon.com>
    
    CR: https://cr.amazon.com/r/8321912/

--- arch/x86/include/asm/kvm_para.h
+++ arch/x86/include/asm/kvm_para.h
@@ -88,6 +88,7 @@ static inline long kvm_hypercall4(unsigned int nr, unsigned long p1,
 #ifdef CONFIG_KVM_GUEST
 bool kvm_para_available(void);
 unsigned int kvm_arch_para_features(void);
+unsigned int kvm_arch_para_hints(void);
 void __init kvm_guest_init(void);
 void kvm_async_pf_task_wait(u32 token, int interrupt_kernel);
 void kvm_async_pf_task_wake(u32 token);
@@ -113,6 +114,11 @@ static inline bool kvm_para_available(void)
 }
 
 static inline unsigned int kvm_arch_para_features(void)
+{
+	return 0;
+}
+
+static inline unsigned int kvm_arch_para_hints(void)
 {
 	return 0;
 }
