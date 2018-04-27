62700cd8ccc7cdce05826d699e2c0e8d59007c3c
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2018-04-27 16:48:01 -0500

    x86/CPU/AMD: Derive CPU topology from CPUID function 0xB when available
    [upstream 3986a0a805e668a63fac0ca2cdfa8db951f87c4b with changes
    after 4.14.62 rebase and backport of 95f3d39ccf7aaea79d1ffdac1c887c2e100ec1b6
    was broken for this patch series]
    
    Derive topology information from Extended Topology Enumeration (CPUID
    function 0xB) when the information is available.
    
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1524865681-112110-3-git-send-email-suravee.suthikulpanit@amd.com
    
    Signed-off-by: Balbir Singh <sblbir@amazon.com>

--- arch/x86/kernel/cpu/topology.c
+++ arch/x86/kernel/cpu/topology.c
@@ -59,7 +59,7 @@ int detect_extended_topology_early(struct cpuinfo_x86 *c)
  * exists, use it for populating initial_apicid and cpu topology
  * detection.
  */
-void detect_extended_topology(struct cpuinfo_x86 *c)
+int detect_extended_topology(struct cpuinfo_x86 *c)
 {
 #ifdef CONFIG_SMP
 	unsigned int eax, ebx, ecx, edx, sub_index;
@@ -67,7 +67,7 @@ void detect_extended_topology(struct cpuinfo_x86 *c)
 	unsigned int core_select_mask, core_level_siblings;
 
 	if (detect_extended_topology_early(c) < 0)
-		return;
+		return -1;
 
 	/*
 	 * Populate HT related information from sub-leaf level 0.
@@ -104,4 +104,5 @@ void detect_extended_topology(struct cpuinfo_x86 *c)
 
 	c->x86_max_cores = (core_level_siblings / smp_num_siblings);
 #endif
+	return 0;
 }
