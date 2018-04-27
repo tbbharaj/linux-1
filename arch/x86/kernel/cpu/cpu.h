ee7ee67a17ec7f7d9e0c65bcf45f2f109bafe383
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2018-04-27 16:48:01 -0500

    x86/CPU/AMD: Derive CPU topology from CPUID function 0xB when available
    [upstream 3986a0a805e668a63fac0ca2cdfa8db951f87c4b with changes
    after 4.14.62 rebase and backport of 95f3d39ccf7aaea79d1ffdac1c887c2e100ec1b6
    was broken for this patch series]
    
    Derive topology information from Extended Topology Enumeration (CPUID
    function 0xB) when the information is available.
    
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1524865681-112110-3-git-send-email-suravee.suthikulpanit@amd.com
    
    Signed-off-by: Balbir Singh <sblbir@amazon.com>

--- arch/x86/kernel/cpu/cpu.h
+++ arch/x86/kernel/cpu/cpu.h
@@ -64,6 +64,7 @@ static inline void tsx_init(void) { }
 extern void get_cpu_cap(struct cpuinfo_x86 *c);
 extern void cpu_detect_cache_sizes(struct cpuinfo_x86 *c);
 extern int detect_extended_topology_early(struct cpuinfo_x86 *c);
+extern int detect_extended_topology(struct cpuinfo_x86 *c);
 extern int detect_ht_early(struct cpuinfo_x86 *c);
 
 unsigned int aperfmperf_get_khz(int cpu);
