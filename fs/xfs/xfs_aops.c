eb3c00e1c6b4354c02c3c52b0e64374f8724a477
Darrick J. Wong <darrick.wong@oracle.com>
2018-05-10 08:38:15 -0700

    iomap: add a swapfile activation function
    Add a new iomap_swapfile_activate function so that filesystems can
    activate swap files without having to use the obsolete and slow bmap
    function.  This enables XFS to support fallocate'd swap files and
    swap files on realtime devices.
    Backport notes:
    1. Changed iomap->addr to reuse existing iomap->blkno. This is ok
    since former is staright up conversion of internal usage of units
    2. Dropped IOMAP_F_DATA_INLINE, IOMAP_NULL_ADDR as the feature is not
    supported in this kernel version
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Anchal Agarwal <anchalag@amazon.com>
    Reviewed-by: Frank van der Linden <fllinden@amazon.com>
    Reviewed-by: Vallish Vaidyeshwara <vallish@amazon.com>
    
    CR: https://code.amazon.com/reviews/CR-3697108

--- fs/xfs/xfs_aops.c
+++ fs/xfs/xfs_aops.c
@@ -1478,6 +1478,16 @@ xfs_vm_set_page_dirty(
 	return newly_dirty;
 }
 
+static int
+xfs_iomap_swapfile_activate(
+	struct swap_info_struct		*sis,
+	struct file			*swap_file,
+	sector_t			*span)
+{
+	sis->bdev = xfs_find_bdev_for_inode(file_inode(swap_file));
+	return iomap_swapfile_activate(sis, swap_file, span, &xfs_iomap_ops);
+}
+
 const struct address_space_operations xfs_address_space_operations = {
 	.readpage		= xfs_vm_readpage,
 	.readpages		= xfs_vm_readpages,
@@ -1491,4 +1501,5 @@ const struct address_space_operations xfs_address_space_operations = {
 	.migratepage		= buffer_migrate_page,
 	.is_partially_uptodate  = block_is_partially_uptodate,
 	.error_remove_page	= generic_error_remove_page,
+	.swap_activate		= xfs_iomap_swapfile_activate,
 };
