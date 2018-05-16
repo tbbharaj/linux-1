015f547e87adb54e27e1f36cdd7ab17aea4898a9
Omar Sandoval <osandov@fb.com>
2018-05-16 11:13:34 -0700

    iomap: provide more useful errors for invalid swap files
    Currently, for an invalid swap file, we print the same error message
    regardless of the reason. This isn't very useful for an admin, who will
    likely want to know why exactly they can't use their swap file. So,
    let's add specific error messages for each reason, and also move the
    bdev check after the flags checks, since the latter are more
    fundamental.
    Backport Notes: This commit had been backported partially
    
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Omar Sandoval <osandov@fb.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Anchal Agarwal <anchalag@amazon.com>
    Reviewed-by: Frank van der Linden <fllinden@amazon.com>
    Reviewed-by: Vallish Vaidyeshwara <vallish@amazon.com>
    
    CR: https://code.amazon.com/reviews/CR-3697108

--- fs/iomap.c
+++ fs/iomap.c
@@ -1173,18 +1173,26 @@ static loff_t iomap_swapfile_activate_actor(struct inode *inode, loff_t pos,
 	/* Skip holes. */
 	if (iomap->type == IOMAP_HOLE)
 		goto out;
-
-	/* Only one bdev per swap file. */
-	if (iomap->bdev != isi->sis->bdev)
-		goto err;
-
+	
 	/* Only real or unwritten extents. */
-	if (iomap->type != IOMAP_MAPPED && iomap->type != IOMAP_UNWRITTEN)
-		goto err;
-
-	/* No uncommitted metadata or shared blocks or inline data. */
-	if (iomap->flags & (IOMAP_F_DIRTY | IOMAP_F_SHARED))
-		goto err;
+	if (iomap->type != IOMAP_MAPPED && iomap->type != IOMAP_UNWRITTEN) {
+		pr_err("swapon: file has unallocated extents\n");
+		return -EINVAL;
+        }
+	/* No uncommitted metadata or shared blocks. */
+	if (iomap->flags & IOMAP_F_DIRTY) {
+		pr_err("swapon: file is not committed\n");
+		return -EINVAL;
+	}  
+	if (iomap->flags & IOMAP_F_SHARED) {
+		pr_err("swapon: file has shared extents\n");
+		return -EINVAL;
+        }
+        /* Only one bdev per swap file. */
+        if (iomap->bdev != isi->sis->bdev) {
+		pr_err("swapon: file is on multiple devices\n");
+		return -EINVAL;
+        }
 
 	if (isi->iomap.length == 0) {
 		/* No accumulated extent, so just store it. */
@@ -1201,9 +1209,6 @@ static loff_t iomap_swapfile_activate_actor(struct inode *inode, loff_t pos,
 	}
 out:
 	return count;
-err:
-	pr_err("swapon: file cannot be used for swap\n");
-	return -EINVAL;
 }
 
 /*
