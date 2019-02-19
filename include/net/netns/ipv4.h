018aca2a8671c83d26cbd583af6ffb3ae7b5b027
Eric Dumazet <edumazet@google.com>
2019-02-19 21:47:29 +0000

    tcp: Namespace-ify sysctl_tcp_rmem and sysctl_tcp_wmem
    [ Upstream commit 356d1833b638bd465672aefeb71def3ab93fc17d ]
    
    Note that when a new netns is created, it inherits its
    sysctl_tcp_rmem and sysctl_tcp_wmem from initial netns.
    
    This change is needed so that we can refine TCP rcvbuf autotuning,
    to take RTT into consideration.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Wei Wang <weiwan@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    [alakeshh: backport to v4.14: The patch does not apply to v4.14
    directly and hence needed manual backport. Function signature for
    the function tcp_select_initial_window had to be changed to be able
    to pass pointer to struct sock. Additional struct members are added
    as per existing convention, and hence ABI compatibility is not
    guaranteed]
    Signed-off-by: Alakesh Haloi <alakeshh@amazon.com>
    Reviewed-by: Balbir Singh <sblbir@amazon.com>
    Reviewed-by: Vallish Vaidyeshwara <vallish@amazon.com>
    Tested-by: Hazem Mohamed Abuelfotoh <abuehaze@amazon.com>

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -128,6 +128,8 @@ struct netns_ipv4 {
 	int sysctl_tcp_sack;
 	int sysctl_tcp_window_scaling;
 	int sysctl_tcp_timestamps;
+	int sysctl_tcp_wmem[3];
+	int sysctl_tcp_rmem[3];
 	struct inet_timewait_death_row tcp_death_row;
 	int sysctl_max_syn_backlog;
 
