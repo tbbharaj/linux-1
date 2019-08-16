a9f32e8bb94e98b6043065aa4c0b0d9b7a398c1f
Jianchao Wang <jianchao.w.wang@oracle.com>
2019-08-16 20:53:09 +0000

    nvme: change namespaces_mutext to namespaces_rwsem
    namespaces_mutext is used to synchronize the operations on ctrl
    namespaces list. Most of the time, it is a read operation.
    
    On the other hand, there are many interfaces in nvme core that
    need this lock, such as nvme_wait_freeze, and even more interfaces
    will be added. If we use mutex here, circular dependency could be
    introduced easily. For example:
    context A                  context B
    nvme_xxx                   nvme_xxx
    hold namespaces_mutext     require namespaces_mutext
    sync context B
    
    So it is better to change it from mutex to rwsem.
    
    Reviewed-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Jianchao Wang <jianchao.w.wang@oracle.com>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Balbir Singh <sblbir@amzn.com>

--- drivers/nvme/host/nvme.h
+++ drivers/nvme/host/nvme.h
@@ -137,7 +137,7 @@ struct nvme_ctrl {
 	struct blk_mq_tag_set *tagset;
 	struct blk_mq_tag_set *admin_tagset;
 	struct list_head namespaces;
-	struct mutex namespaces_mutex;
+	struct rw_semaphore namespaces_rwsem;
 	struct device *device;	/* char device */
 	struct list_head node;
 	struct ida ns_ida;
