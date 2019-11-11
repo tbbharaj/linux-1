5f7aa8cd1a0e3a0e394dc302ac29da84910f05f0
Max Gurtovoy <maxg@mellanox.com>
2019-11-11 01:51:04 +0000

    nvme: rename NVME_CTRL_RECONNECTING state to NVME_CTRL_CONNECTING
    In pci transport, this state is used to mark the initialization
    process. This should be also used in other transports as well.
    
    Signed-off-by: Max Gurtovoy <maxg@mellanox.com>
    Reviewed-by: James Smart <james.smart@broadcom.com>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    
    [upstream  ad6a0a52e6de3d1161b7999c7903db906ba4cf79]
    [Balbir, patch does not apply cleanly due to missing fc and rdma
    patches and ADMIN_ONLY state, currently we support only PCI
    controllers, supporting fc and rdma is out of scope]
    
    Signed-off-by: Balbir Singh <sblbir@amazon.com>
    Reviewed-by: Anchal Agarwal <anchalag@amazon.com>
    Reviewed-by: Frank van der Linden <fllinden@amazon.com>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -186,7 +186,7 @@ bool nvme_change_ctrl_state(struct nvme_ctrl *ctrl,
 		switch (old_state) {
 		case NVME_CTRL_NEW:
 		case NVME_CTRL_RESETTING:
-		case NVME_CTRL_RECONNECTING:
+		case NVME_CTRL_CONNECTING:
 			changed = true;
 			/* FALLTHRU */
 		default:
@@ -203,7 +203,7 @@ bool nvme_change_ctrl_state(struct nvme_ctrl *ctrl,
 			break;
 		}
 		break;
-	case NVME_CTRL_RECONNECTING:
+	case NVME_CTRL_CONNECTING:
 		switch (old_state) {
 		case NVME_CTRL_LIVE:
 		case NVME_CTRL_RESETTING:
@@ -217,7 +217,7 @@ bool nvme_change_ctrl_state(struct nvme_ctrl *ctrl,
 		switch (old_state) {
 		case NVME_CTRL_LIVE:
 		case NVME_CTRL_RESETTING:
-		case NVME_CTRL_RECONNECTING:
+		case NVME_CTRL_CONNECTING:
 			changed = true;
 			/* FALLTHRU */
 		default:
@@ -2217,7 +2217,7 @@ static ssize_t nvme_sysfs_show_state(struct device *dev,
 		[NVME_CTRL_NEW]		= "new",
 		[NVME_CTRL_LIVE]	= "live",
 		[NVME_CTRL_RESETTING]	= "resetting",
-		[NVME_CTRL_RECONNECTING]= "reconnecting",
+		[NVME_CTRL_CONNECTING]	= "connecting",
 		[NVME_CTRL_DELETING]	= "deleting",
 		[NVME_CTRL_DEAD]	= "dead",
 	};
