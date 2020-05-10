fe4b9f54862ae0781a4d6263b5d5a4e7aae43d46
Balbir Singh <sblbir@amazon.com>
2020-05-10 11:48:01 +1000

    x86/kvm: Refactor L1D flushing
    linux-next commit id 3f768f0032dbc0657ed7e48f4735a3c4e49e25d7
    
    Move more L1D flush related code out of KVM/VMX into builtin code to allow
    reuse for L1D flushing:
    
     - Move the initialization to l1d_flush_init_once() and remove the
       deallocation of the L1D flush pages.
    
       This avoids adding complex refcounting of users (VMX or tasks which
       opt into a L1D flush on context switch) for the price of a few pages
       potentially wasted when no users are left.
    
     - Unify the flush invocations as arch_l1d_flush() which attempts
       hardware flushing and falls back to the software implementation
       with the option of prepopulating the TLB entries first.
    
    [ tglx: Massage changelog and add a paranoid check of the flush pages
      	pointer ]
    
    Suggested-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Balbir Singh <sblbir@amazon.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/20200510014803.12190-5-sblbir@amazon.com

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -201,8 +201,6 @@ static const struct {
 	[VMENTER_L1D_FLUSH_NOT_REQUIRED] = {"not required", false},
 };
 
-static void *vmx_l1d_flush_pages;
-
 static int vmx_setup_l1d_flush(enum vmx_l1d_flush_state l1tf)
 {
 	if (!boot_cpu_has_bug(X86_BUG_L1TF)) {
@@ -245,12 +243,9 @@ static int vmx_setup_l1d_flush(enum vmx_l1d_flush_state l1tf)
 		l1tf = VMENTER_L1D_FLUSH_ALWAYS;
 	}
 
-	if (l1tf != VMENTER_L1D_FLUSH_NEVER && !vmx_l1d_flush_pages &&
-	    !boot_cpu_has(X86_FEATURE_FLUSH_L1D)) {
-		vmx_l1d_flush_pages = l1d_flush_alloc_pages();
-		if (!vmx_l1d_flush_pages)
+	if (l1tf != VMENTER_L1D_FLUSH_NEVER)
+		if (l1d_flush_init_once())
 			return -ENOMEM;
-	}
 
 	l1tf_vmx_mitigation = l1tf;
 
@@ -5978,12 +5973,7 @@ static void vmx_l1d_flush(struct kvm_vcpu *vcpu)
 	}
 
 	vcpu->stat.l1d_flush++;
-
-	if (!l1d_flush_hw())
-		return;
-
-	l1d_flush_populate_tlb(vmx_l1d_flush_pages);
-	l1d_flush_sw(vmx_l1d_flush_pages);
+	arch_l1d_flush(L1D_FLUSH_POPULATE_TLB);
 }
 
 static void update_cr8_intercept(struct kvm_vcpu *vcpu, int tpr, int irr)
@@ -7896,10 +7886,6 @@ static struct kvm_x86_ops vmx_x86_ops __ro_after_init = {
 
 static void vmx_cleanup_l1d_flush(void)
 {
-	if (vmx_l1d_flush_pages) {
-		l1d_flush_cleanup_pages(vmx_l1d_flush_pages);
-		vmx_l1d_flush_pages = NULL;
-	}
 	/* Restore state so sysfs ignores VMX */
 	l1tf_vmx_mitigation = VMENTER_L1D_FLUSH_AUTO;
 }
