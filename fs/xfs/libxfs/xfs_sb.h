dd6c4270012aa0f3b07253bfbf476eeb31355681
Andy Strohman <astroh@amazon.com>
2020-07-08 20:09:04 +0000

    xfs: add prerequisite for online labeling
    Add function xfs_update_secondary_sbs from commit:
    b16817b66b6c97d2a812d663d26faed40079892a
    This function is all that is required for a online labeling patch
    that follows. Picking the commit in its entirety triggered conflicts
    which seemed like more trouble than it's worth.
    
    Signed-off-by: Andy Strohman <astroh@amazon.com>

--- fs/xfs/libxfs/xfs_sb.h
+++ fs/xfs/libxfs/xfs_sb.h
@@ -33,5 +33,6 @@ extern void	xfs_sb_mount_common(struct xfs_mount *mp, struct xfs_sb *sbp);
 extern void	xfs_sb_from_disk(struct xfs_sb *to, struct xfs_dsb *from);
 extern void	xfs_sb_to_disk(struct xfs_dsb *to, struct xfs_sb *from);
 extern void	xfs_sb_quota_from_disk(struct xfs_sb *sbp);
+extern int      xfs_update_secondary_sbs(struct xfs_mount *mp);
 
 #endif	/* __XFS_SB_H__ */
