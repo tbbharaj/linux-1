083517e4717143ae3cdba077f178de896296cf59
Ethan Chen <yishache@amazon.com>
2020-08-27 18:23:40 +0000

    drivers/amazon: efa: update to 1.9.0
    Signed-off-by: Ethan Chen <yishache@amazon.com>

--- drivers/amazon/net/efa/kcompat.h
+++ drivers/amazon/net/efa/kcompat.h
@@ -1,121 +1,22 @@
 /* SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause */
 /*
- * Copyright 2018-2019 Amazon.com, Inc. or its affiliates. All rights reserved.
+ * Copyright 2018-2020 Amazon.com, Inc. or its affiliates. All rights reserved.
  */
 
 #ifndef _KCOMPAT_H_
 #define _KCOMPAT_H_
 
-#ifndef LINUX_VERSION_CODE
-#include <linux/version.h>
-#else
-#define KERNEL_VERSION(a, b, c) (((a) << 16) + ((b) << 8) + (c))
-#endif
+#include "config.h"
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,33))
-#include <linux/utsrelease.h>
-#else
-#include <generated/utsrelease.h>
-#endif
-
-/******************************************************************************/
-/**************************** RHEL macros *************************************/
-/******************************************************************************/
-
-#ifndef RHEL_RELEASE_VERSION
-#define RHEL_RELEASE_VERSION(a, b) (((a) << 8) + (b))
-#endif
-
-#ifndef RHEL_RELEASE_CODE
-#define RHEL_RELEASE_CODE 0
-#endif
-
-/*****************************************************************************/
-/* Start of upstream defines */
-#if ((LINUX_VERSION_CODE >= KERNEL_VERSION(3,15,0)) || \
-	(RHEL_RELEASE_CODE && \
-	(RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7,4))))
-#define HAVE_UMEM_SCATTERLIST_IF
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,2,0) || \
-	(RHEL_RELEASE_CODE && \
-	(RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7,0)))
-#define HAVE_GET_PORT_IMMUTABLE
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,10,0) || \
-	(RHEL_RELEASE_CODE && \
-	(RHEL_RELEASE_CODE > RHEL_RELEASE_VERSION(7,0)))
-#define HAVE_CREATE_AH_UDATA
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,2,0) || \
-	(RHEL_RELEASE_CODE && \
-	(RHEL_RELEASE_CODE > RHEL_RELEASE_VERSION(7,0)))
-#define HAVE_IB_QUERY_DEVICE_UDATA
-#define HAVE_CREATE_CQ_ATTR
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 7, 0)
-#define HAVE_HW_STATS
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,12,0) || \
-	(RHEL_RELEASE_CODE && RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7,6))
-#define HAVE_CREATE_AH_RDMA_ATTR
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0) || \
-	(RHEL_RELEASE_CODE && RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7,6))
-#define HAVE_DEV_PARENT
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
-#define HAVE_IB_DEV_OPS
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
-#define HAVE_CREATE_DESTROY_AH_FLAGS
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 1, 0)
-#define HAVE_SG_DMA_PAGE_ITER
-#define HAVE_PD_CORE_ALLOCATION
-#define HAVE_UCONTEXT_CORE_ALLOCATION
-#define HAVE_NO_KVERBS_DRIVERS
-#define HAVE_IB_UMEM_GET_UDATA
-#define HAVE_UDATA_TO_DRV_CONTEXT
-#define HAVE_IB_REGISTER_DEVICE_TWO_PARAMS
-#define HAVE_SAFE_IB_ALLOC_DEVICE
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 2, 0)
-#define HAVE_AH_CORE_ALLOCATION
-#define HAVE_ALLOC_PD_NO_UCONTEXT
-#define HAVE_CREATE_CQ_NO_UCONTEXT
-#define HAVE_DEALLOC_PD_UDATA
-#define HAVE_DEREG_MR_UDATA
-#define HAVE_DESTROY_CQ_UDATA
-#define HAVE_DESTROY_QP_UDATA
-#define HAVE_IB_UMEM_FIND_SINGLE_PG_SIZE
-#define HAVE_UPSTREAM_EFA
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 3, 0)
-#define HAVE_IB_DEVICE_OPS_COMMON
-#define HAVE_IB_VOID_DESTROY_CQ
-#define HAVE_CQ_CORE_ALLOCATION
-#endif
-
-/* End of upstream defines */
-
-#if !defined(HAVE_CREATE_AH_UDATA) || !defined(HAVE_IB_QUERY_DEVICE_UDATA)
+#if defined(HAVE_CREATE_AH_NO_UDATA) || !defined(HAVE_IB_QUERY_DEVICE_UDATA)
 #define HAVE_CUSTOM_COMMANDS
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,5,0) && \
-	(RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(7,0))
+#ifndef ALIGN_DOWN
+#define ALIGN_DOWN(x, a)	__ALIGN_KERNEL((x) - ((a) - 1), (a))
+#endif
+
+#ifndef HAVE_IB_IS_UDATA_CLEARED
 #include <linux/string.h>
 #include <linux/slab.h>
 #include <rdma/ib_verbs.h>
@@ -146,12 +47,15 @@ free:
 }
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,16,0) && \
-	RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(7,6)
+#ifndef HAVE_IB_QPT_DRIVER
 #define IB_QPT_DRIVER 0xFF
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 2, 0)
+#if defined(HAVE_DRIVER_ID) && !defined(HAVE_UPSTREAM_EFA)
+#define RDMA_DRIVER_EFA 17
+#endif
+
+#ifndef HAVE_IBDEV_PRINT
 #define ibdev_err(_ibdev, format, arg...) \
 	dev_err(&((struct ib_device *)(_ibdev))->dev, format, ##arg)
 #define ibdev_dbg(_ibdev, format, arg...) \
@@ -162,7 +66,7 @@ free:
 	dev_info(&((struct ib_device *)(_ibdev))->dev, format, ##arg)
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 4, 0)
+#ifndef HAVE_IBDEV_PRINT_RATELIMITED
 #define ibdev_err_ratelimited(_ibdev, format, arg...) \
 	dev_err_ratelimited(&((struct ib_device *)(_ibdev))->dev, format, ##arg)
 #define ibdev_dbg_ratelimited(_ibdev, format, arg...) \
@@ -173,8 +77,7 @@ free:
 	dev_info_ratelimited(&((struct ib_device *)(_ibdev))->dev, format, ##arg)
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0) && \
-	RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(7, 6)
+#ifndef HAVE_KVZALLOC
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
 
@@ -188,6 +91,65 @@ static inline void *kvzalloc(size_t size, gfp_t flags)
 
 	return vzalloc(size);
 }
+#endif
+
+#ifndef HAVE_IB_PORT_PHYS_STATE_LINK_UP
+#define IB_PORT_PHYS_STATE_LINK_UP 5
+#endif
+
+#ifndef HAVE_CORE_MMAP_XA
+#include <linux/types.h>
+#include <linux/device.h>
+
+struct rdma_user_mmap_entry {
+	struct ib_ucontext *ucontext;
+	unsigned long start_pgoff;
+	size_t npages;
+};
+
+/* Return the offset (in bytes) the user should pass to libc's mmap() */
+static inline u64
+rdma_user_mmap_get_offset(const struct rdma_user_mmap_entry *entry)
+{
+	return (u64)entry->start_pgoff << PAGE_SHIFT;
+}
+
+/*
+ * Backported kernels don't keep refcnt on entries, hence they should not
+ * be removed.
+ */
+static inline void
+rdma_user_mmap_entry_remove(struct rdma_user_mmap_entry *entry)
+{
+}
+
+static inline void rdma_user_mmap_entry_put(struct rdma_user_mmap_entry *entry)
+{
+}
+#endif
+
+#ifndef sizeof_field
+#define sizeof_field(TYPE, MEMBER) sizeof((((TYPE *)0)->MEMBER))
+#endif
+
+#ifndef HAVE_BITFIELD_H
+#define __bf_shf(x) (__builtin_ffsll(x) - 1)
+
+#define FIELD_PREP(_mask, _val)                                         \
+	({                                                              \
+		((typeof(_mask))(_val) << __bf_shf(_mask)) & (_mask);   \
+	})
+
+#define FIELD_GET(_mask, _reg)                                          \
+	({                                                              \
+		(typeof(_mask))(((_reg) & (_mask)) >> __bf_shf(_mask)); \
+	})
+#endif
+
+#ifndef HAVE_RDMA_NODE_UNSPECIFIED
+enum {
+	RDMA_NODE_UNSPECIFIED = 7,
+};
 #endif
 
 #endif /* _KCOMPAT_H_ */
