7bef8618b434d33403507a755402cfe8460f02b8
Vasily Gorbik <gor@linux.ibm.com>
2020-11-13 00:03:29 +0100

    objtool: Fix x86 orc generation on big endian cross-compiles
    Correct objtool orc generation endianness problems to enable fully
    functional x86 cross-compiles on big endian hardware.
    
    Introduce bswap_if_needed() macro, which does a byte swap if target
    endianness doesn't match the host, i.e. cross-compilation for little
    endian on big endian and vice versa.  The macro is used for conversion
    of multi-byte values which are read from / about to be written to a
    target native endianness ELF file.
    
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    (cherry picked from commit 8bfe273238d77d3cee18e4c03b2f26ae360b5661)

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -13,6 +13,7 @@
 #include "special.h"
 #include "warn.h"
 #include "arch_elf.h"
+#include "endianness.h"
 
 #include <linux/objtool.h>
 #include <linux/hashtable.h>
@@ -1456,7 +1457,7 @@ static int read_unwind_hints(struct objtool_file *file)
 		cfa = &insn->cfi.cfa;
 
 		if (hint->type == UNWIND_HINT_TYPE_RET_OFFSET) {
-			insn->ret_offset = hint->sp_offset;
+			insn->ret_offset = bswap_if_needed(hint->sp_offset);
 			continue;
 		}
 
@@ -1468,7 +1469,7 @@ static int read_unwind_hints(struct objtool_file *file)
 			return -1;
 		}
 
-		cfa->offset = hint->sp_offset;
+		cfa->offset = bswap_if_needed(hint->sp_offset);
 		insn->cfi.type = hint->type;
 		insn->cfi.end = hint->end;
 	}
