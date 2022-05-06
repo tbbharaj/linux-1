fca11c4734c209a791f31d3cc6ea9177fb068cc4
tbbharaj <tbbharaj@amazon.com>
2022-05-06 20:39:16 +0000

    add arm sched_clock implementation

--- arch/arm64/kernel/perf_event.c
+++ arch/arm64/kernel/perf_event.c
@@ -1258,7 +1258,7 @@ device_initcall(armv8_pmu_driver_init)
 void arch_perf_update_userpage(struct perf_event *event,
 			       struct perf_event_mmap_page *userpg, u64 now)
 {
-	struct clock_read_data *rd;
+/*	struct clock_read_data *rd;
 	unsigned int seq;
 	u64 ns;
 
@@ -1277,35 +1277,36 @@ void arch_perf_update_userpage(struct perf_event *event,
 		userpg->time_zero = rd->epoch_ns;
 		userpg->time_cycles = rd->epoch_cyc;
 		userpg->time_mask = rd->sched_clock_mask;
-
+*/
 		/*
 		 * Subtract the cycle base, such that software that
 		 * doesn't know about cap_user_time_short still 'works'
 		 * assuming no wraps.
 		 */
-		ns = mul_u64_u32_shr(rd->epoch_cyc, rd->mult, rd->shift);
+/*		ns = mul_u64_u32_shr(rd->epoch_cyc, rd->mult, rd->shift);
 		userpg->time_zero -= ns;
 
 	} while (sched_clock_read_retry(seq));
 
 	userpg->time_offset = userpg->time_zero - now;
-
+*/
 	/*
 	 * time_shift is not expected to be greater than 31 due to
 	 * the original published conversion algorithm shifting a
 	 * 32-bit value (now specifies a 64-bit value) - refer
 	 * perf_event_mmap_page documentation in perf_event.h.
 	 */
-	if (userpg->time_shift == 32) {
+/*	if (userpg->time_shift == 32) {
 		userpg->time_shift = 31;
 		userpg->time_mult >>= 1;
 	}
-
+*/
 	/*
 	 * Internal timekeeping for enabled/running/stopped times
 	 * is always computed with the sched_clock.
 	 */
-	userpg->cap_user_time = 1;
+/*	userpg->cap_user_time = 1;
 	userpg->cap_user_time_zero = 1;
 	userpg->cap_user_time_short = 1;
+*/
 }
